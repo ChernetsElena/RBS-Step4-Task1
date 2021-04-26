52348a5ca854249f742f037a3baa3b17876839e8
ChernetsElena <chernets-elena@mail.ru>
2021-04-26 15:55:30 +0200

    start task

---
+++ create_script.sql
@@ -0,0 +1,28 @@
+CREATE SCHEMA partition;
+
+CREATE TABLE partition.t_employees (
+    pk_id serial,
+    c_lastname varchar(80),
+    c_firstname varchar,
+    c_middlename varchar,
+    c_salary money,
+    c_email varchar,
+    c_position varchar
+);
+
+CREATE TABLE partition.t_clients (
+    pk_id serial,
+    c_lastname varchar,
+    c_firstname varchar,
+    c_middlename varchar,
+    c_email varchar
+);
+
+CREATE SCHEMA auth;
+
+CREATE TABLE auth.t_user (
+    pk_id serial,
+    c_login varchar,
+    c_password bytea,
+    c_last_in timestamp
+);
